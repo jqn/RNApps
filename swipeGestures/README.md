e3763f47bdeae40fe5a0f7c21ebfc432c543b6d5
jqn <jqn@Macbooks-MacBook-Pro.local>
2016-11-23 14:23:58 -0700

    Added simplegesture logs to react according to swipe direction

---
+++ swipeGestures/README.md
@@ -0,0 +1,6 @@
+This example uses decorators which are not longer supported by Babel 6 after
+React Native version 0.22.
+
+This example is using the workaround described in this article:
+
+http://moduscreate.com/using-es2016-decorators-in-react-native/
--- swipeGestures/temp.js
+++ swipeGestures/temp.js
@@ -31,21 +31,55 @@ export default class swipeGestures extends Component {
 
   componentWillMount() {
     this._panResponder = PanResponder.create({
-      onMoveShouldSetPanResponder: (e, gestureState) => {
+      onMoveShouldSetResponderCapture: () => true,
+      onMoveShouldSetPanResponderCapture: () => true,
+
+      onPanResponderGrant: (e, gestureState) => {
+        // Set the initial value to the current state
+        this.state.pan.setOffset({x: this.state.pan.x._value, y: this.state.pan.y._value});
+        this.state.pan.setValue({x: 0, y: 0});
+        this.isVertical();
+        Animated.spring(
+          this.state.scale,
+          { toValue: 1.1, friction: 3 }
+        ).start();
+
         let sgs = new SimpleGesture(e, gestureState);
-        console.log('------------------');
+
         console.log('vertical', sgs.isVertical());
         console.log('horizontal', sgs.isHorizontal());
         console.log('isLongSwipeLeft', sgs.isLongSwipeLeft());
-        console.log('isSwipeLeft', sgs.isSwipeLeft());
-        console.log('isSwipeRight', sgs.isSwipeRight());
-        console.log('isSwipeUp', sgs.isSwipeUp());
-        console.log('isSwipeDown', sgs.isSwipeDown());
+      },
+
+      // When we drag/pan the object, set the delate to the states pan position
+      onPanResponderMove: Animated.event([
+        null, {dx: this.state.pan.x, dy: this.state.pan.y},
+      ]),
+
+      onPanResponderRelease: (e, {vx, vy}) => {
+        // Flatten the offset to avoid erratic behavior
+        this.state.pan.flattenOffset();
+        Animated.spring(
+          this.state.scale,
+          { toValue: 1, friction: 3 }
+        ).start();
       },
     });
   }
 
+  isVertical() {
+    console.log()
+  }
+
   render() {
+    let { pan, scale } = this.state;
+
+    let [translateX, translateY] = [pan.x, pan.y];
+
+    let rotate = '0deg';
+
+    let imageStyle = {transform: [{translateX}, {translateY}, {rotate}, {scale}]};
+
     return (
       <View style={styles.container} {...this._panResponder.panHandlers}>
           <Image source={require('./assets/panresponder.png')} />
